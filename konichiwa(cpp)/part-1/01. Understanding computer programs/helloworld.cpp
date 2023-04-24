cdaadf2f3e41b0510e498fe61327ea0308ab80dc
Ghostopopop <95743994+Ghostopopop@users.noreply.github.com>
2023-04-24 13:31:33 +0530

    new

---
+++ konichiwa(cpp)/part-1/01. Understanding computer programs/helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    // clrscr();
+    cout<<"Hello World012"<<endl;
+    cout<<"git";
+    return 0;
+}
